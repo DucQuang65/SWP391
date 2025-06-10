55816021f7ecfa74871ece4ed43b9069fe14c43d
QuangVinh108 <winkiwar7239@gmail.com>
2025-06-10 18:03:38 +0700

    Update UserDto.cs

--- Hien_mau/Hien_mau/Dto/UserDto.cs
+++ Hien_mau/Hien_mau/Dto/UserDto.cs
@@ -6,7 +6,10 @@ namespace Hien_mau.Dto
     {
         [Required, EmailAddress]
         public string Email { get; set; } = null!;
-
+        [Required]
+        [MinLength(6)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{6,}$",
+        ErrorMessage = "Mật khẩu phải có ít nhất 6 ký tự, gồm chữ thường, chữ hoa, số và ký tự đặc biệt.")]
         public string? Password { get; set; } // Used for client-side registration
         //public string IdToken { get; set; } = null!; // Sent by client after Firebase auth
     }
