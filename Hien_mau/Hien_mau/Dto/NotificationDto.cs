8812225796f7a7f68702a577aee3a4486993e9cf
DucQuang65 <155624683+DucQuang65@users.noreply.github.com>
2025-07-07 23:44:22 +0700

    hoàn thành noti

---
+++ Hien_mau/Hien_mau/Dto/NotificationDto.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Hien_mau.Dto
+{
+    public class NotificationDto
+    {
+        public string? Title { get; set; }
+        public string? Message { get; set; }
+        public string? Type { get; set; }
+        public int UserId { get; set; }
+        public byte Priority { get; set; }
+        public bool IsRead { get; set; } = false;
+        public DateTime SentAt { get; set; } = DateTime.Now;
+        
+    }
+
+    public class NotificationUpdateDto
+    {
+        public string? Title { get; set; }
+        public string? Message { get; set; }
+        public string? Type { get; set; }
+        public int UserId { get; set; }
+        public byte Priority { get; set; }
+        public bool IsRead { get; set; } = false;
+        public DateTime SentAt { get; set; } = DateTime.Now;
+    }
+
+}
