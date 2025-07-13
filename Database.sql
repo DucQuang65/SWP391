3ecefae9a009ab0e3aae396cadab46b11eaf1a4d
DucQuang65 <155624683+DucQuang65@users.noreply.github.com>
2025-07-13 21:31:50 +0700

    Status

--- Database.sql
+++ Database.sql
@@ -181,7 +181,7 @@ CREATE TABLE Appointments (
     DoctorID INT NULL,
     AppointmentDate DATE NOT NULL,
     TimeSlot NVARCHAR(50) NOT NULL,
-    Status TINYINT NOT NULL DEFAULT 0, -- 0: chờ duyệt, 1: từ chối, 2: chấp nhận
+    Status BIT, -- 0:từ chối, 1: chấp nhận
     Process TINYINT NOT NULL DEFAULT 0,--0-- dang ky, 1--kham suc khoe, 2-- lay mau, 3--xet nghiem, 4--nhap kho
     Cancel BIT NOT NULL DEFAULT 0,
     Notes NVARCHAR(255) NULL,
